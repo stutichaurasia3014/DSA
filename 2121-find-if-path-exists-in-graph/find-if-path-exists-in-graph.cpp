13a153a74e043f1b2c52e2e0d82945670389b3b1
Stuti Chaurasia <166703911+stutichaurasia3014@users.noreply.github.com>
2026-08-15 17:23:56 +0530

    Time: 438 ms (24.71%) | Memory: 329.1 MB (23.66%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,18 +1,44 @@
+// class Solution {
+// public:
+// bool check(unordered_map<int,vector<int>> &mp , int S,int D,vector<bool> &visited){
+//     if(S==D)
+//     return true;
+
+//     if(visited[S]==true)
+//     return false;
+//     visited[S]=true;
+//     for(auto &node:mp[S]){
+//         if(check(mp,node,D,visited)==true)
+//         return true;
+//     }
+//     return false;
+// }
+//     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+//         unordered_map<int,vector<int>> mp;
+
+//         for(vector<int> &edge:edges){
+//             int u=edge[0];
+//             int v=edge[1];
+
+//             mp[u].push_back(v);
+//             mp[v].push_back(u);
+//         }
+
+//         vector<bool> visited(n,false);
+
+//         return check(mp,source,destination,visited);
+        
+//     }
+// };
+
+
+
+
+// METHOD 2 USINF BFS
+
+
 class Solution {
 public:
-bool check(unordered_map<int,vector<int>> &mp , int S,int D,vector<bool> &visited){
-    if(S==D)
-    return true;
-
-    if(visited[S]==true)
-    return false;
-    visited[S]=true;
-    for(auto &node:mp[S]){
-        if(check(mp,node,D,visited)==true)
-        return true;
-    }
-    return false;
-}
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         unordered_map<int,vector<int>> mp;
 
@@ -26,7 +52,25 @@ bool check(unordered_map<int,vector<int>> &mp , int S,int D,vector<bool> &visite
 
         vector<bool> visited(n,false);
 
-        return check(mp,source,destination,visited);
+       queue<int>que;
+       que.push(source);
+       visited[source]=true;
+
+       while(!que.empty()){
+        int node=que.front();
+        que.pop();
+
+        if(node==destination) return true;
+
+        for(auto &V:mp[node]){
+            if(!visited[V]){
+                que.push(V);
+                visited[V]=true;
+            }
+        }
+       }
+  return false;
+
         
     }
 };
