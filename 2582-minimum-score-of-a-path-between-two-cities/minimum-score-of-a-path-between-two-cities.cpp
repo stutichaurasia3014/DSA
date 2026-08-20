3374b29d54908aea79834ed133059c57e768e72b
Stuti Chaurasia <166703911+stutichaurasia3014@users.noreply.github.com>
2026-08-20 15:37:49 +0530

    Time: 130 ms (12.25%) | Memory: 191.1 MB (5.08%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -1,25 +1,60 @@
-class Solution {
-public:
 
-    void dfs(unordered_map<int, vector<pair<int, int>>> &adj,
-             int u,
-             vector<bool> &visited,
-             int &result) {
+// METHOD 1 USING DFS
 
-        visited[u] = true;
+// class Solution {
+// public:
 
-        for (auto &P : adj[u]) {
-            int v = P.first;
-            int d = P.second;
+//     void dfs(unordered_map<int, vector<pair<int, int>>> &adj,
+//              int u,
+//              vector<bool> &visited,
+//              int &result) {
+
+//         visited[u] = true;
+
+//         for (auto &P : adj[u]) {
+//             int v = P.first;
+//             int d = P.second;
 
             
-            result = min(result, d);
+//             result = min(result, d);
 
-            if (!visited[v]) {
-                dfs(adj, v, visited, result);
-            }
-        }
-    }
+//             if (!visited[v]) {
+//                 dfs(adj, v, visited, result);
+//             }
+//         }
+//     }
+
+//     int minScore(int n, vector<vector<int>>& roads) {
+
+//         unordered_map<int, vector<pair<int, int>>> adj;
+
+    
+//         for (vector<int> &vec : roads) {
+//             int u = vec[0];
+//             int v = vec[1];
+//             int d = vec[2];
+
+//             adj[u].push_back({v, d});
+//             adj[v].push_back({u, d});
+//         }
+
+//         vector<bool> visited(n + 1, false);
+
+//         int result = INT_MAX;
+
+        
+//         dfs(adj, 1, visited, result);
+
+//         return result;
+//     }
+// };
+
+
+
+// METHOD 2 USING BFS 
+
+class Solution {
+public:
 
     int minScore(int n, vector<vector<int>>& roads) {
 
@@ -36,11 +71,29 @@ public:
         }
 
         vector<bool> visited(n + 1, false);
+        queue<int> que;
+        que.push(1);
+        visited[1]=true;
 
         int result = INT_MAX;
 
         
-        dfs(adj, 1, visited, result);
+      while(!que.empty()){
+        int u=que.front();
+        que.pop();
+
+        for(auto &P:adj[u]){
+            int v=P.first;
+            int d=P.second;
+
+            result=min(result,d);
+
+            if(!visited[v]){
+                visited[v]=true;
+                que.push(v);
+            }
+        }
+      }
 
         return result;
     }
