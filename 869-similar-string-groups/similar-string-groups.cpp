22354b382401cbee6f8d0594ac4aaf396319d1a9
Stuti Chaurasia <166703911+stutichaurasia3014@users.noreply.github.com>
2026-08-21 14:28:51 +0530

    Time: 83 ms (50.52%) | Memory: 17.5 MB (37.33%) - LeetSync

--- 869-similar-string-groups/similar-string-groups.cpp
+++ 869-similar-string-groups/similar-string-groups.cpp
@@ -1,3 +1,57 @@
+// class Solution {
+// public:
+// bool isSimilar(string &s1,string &s2){
+//     int m=s1.length();
+//     int diff=0;
+
+//     for(int i=0;i<m;i++){
+//         if(s1[i] !=s2[i])
+//         diff++;
+//     }
+//     return diff==2 || diff==0;
+// }
+
+// void dfs(int u, unordered_map<int,vector<int>> &adj,vector<bool> &visited){
+//   visited[u]=true;
+
+//   for( int &v:adj[u]){
+//     if(!visited[v]){
+//         dfs(v,adj,visited);
+//     }
+//   }
+
+
+// }
+//     int numSimilarGroups(vector<string>& strs) {
+//         int n =strs.size();
+
+//         unordered_map<int,vector<int>> adj;
+
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(isSimilar(strs[i],strs[j])){
+//                     adj[i].push_back(j);
+//                     adj[j].push_back(i);
+//                 }
+//             }
+//         }
+
+//         vector<bool> visited(n,false);
+
+//         int count=0;
+//         for( int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 dfs(i,adj,visited);
+//                 count++;
+//             }
+//         }
+//      return count;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
 bool isSimilar(string &s1,string &s2){
@@ -11,14 +65,24 @@ bool isSimilar(string &s1,string &s2){
     return diff==2 || diff==0;
 }
 
-void dfs(int u, unordered_map<int,vector<int>> &adj,vector<bool> &visited){
+void bfs(int u, unordered_map<int,vector<int>> &adj,vector<bool> &visited){
+  queue<int> que;
+  que.push(u);
   visited[u]=true;
 
-  for( int &v:adj[u]){
-    if(!visited[v]){
-        dfs(v,adj,visited);
+  while(!que.empty()){
+    int u=que.front();
+    que.pop();
+
+    for(int &v:adj[u]){
+        if(!visited[v]){
+            que.push(v);
+            visited[v]=true;
+        }
     }
   }
+
+
 
 
 }
@@ -41,7 +105,7 @@ void dfs(int u, unordered_map<int,vector<int>> &adj,vector<bool> &visited){
         int count=0;
         for( int i=0;i<n;i++){
             if(!visited[i]){
-                dfs(i,adj,visited);
+                bfs(i,adj,visited);
                 count++;
             }
         }
