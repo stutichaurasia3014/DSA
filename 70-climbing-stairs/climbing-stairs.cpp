acc201d660ef78aece709c2f7b6a9c2779c0b48d
Stuti Chaurasia <166703911+stutichaurasia3014@users.noreply.github.com>
2026-09-14 14:07:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (38.45%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,25 +1,47 @@
+// class Solution {
+// public:
+//     int solve(int n, vector<int>& dp) {
+        
+//         if(n < 0)
+//             return 0;
+        
+//         if(n == 0)
+//             return 1;
+        
+//         if(dp[n] != -1)
+//             return dp[n];
+        
+//         int one_step = solve(n - 1, dp);
+//         int two_step = solve(n - 2, dp);
+        
+//         return dp[n] = one_step + two_step;
+//     }
+
+//     int climbStairs(int n) {
+//         vector<int> dp(n + 1, -1);
+        
+//         return solve(n, dp);
+//     }
+// };
+
+
 class Solution {
 public:
-    int solve(int n, vector<int>& dp) {
-        
-        if(n < 0)
-            return 0;
-        
-        if(n == 0)
-            return 1;
-        
-        if(dp[n] != -1)
-            return dp[n];
-        
-        int one_step = solve(n - 1, dp);
-        int two_step = solve(n - 2, dp);
-        
-        return dp[n] = one_step + two_step;
-    }
-
+     
     int climbStairs(int n) {
+        if(n<=2)
+        return n;
+
         vector<int> dp(n + 1, -1);
+
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
         
-        return solve(n, dp);
+        return  dp[n];
     }
 };
