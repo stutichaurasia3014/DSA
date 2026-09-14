d9dbf340d9d16de21078471ca7546cefba426470
Stuti Chaurasia <166703911+stutichaurasia3014@users.noreply.github.com>
2026-09-14 13:54:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (38.45%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(int n, vector<int>& dp) {
+        
+        if(n < 0)
+            return 0;
+        
+        if(n == 0)
+            return 1;
+        
+        if(dp[n] != -1)
+            return dp[n];
+        
+        int one_step = solve(n - 1, dp);
+        int two_step = solve(n - 2, dp);
+        
+        return dp[n] = one_step + two_step;
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        
+        return solve(n, dp);
+    }
+};
