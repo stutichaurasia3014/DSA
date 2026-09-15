1e712ebc9353c1e7b317f0135e09afbaacb294df
Stuti Chaurasia <166703911+stutichaurasia3014@users.noreply.github.com>
2026-09-15 13:22:16 +0530

    Time: 269 ms (41.55%) | Memory: 157.1 MB (51.50%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,37 +1,63 @@
-class Solution {
-public:
-    int n;
-    vector<vector<long long>> dp;
+// class Solution {
+// public:
+//     int n;
+//     vector<vector<long long>> dp;
 
-    long long solve(int idx, vector<int>& nums, bool flag) {
-        if (idx >= n) {
-            return 0;
-        }
+//     long long solve(int idx, vector<int>& nums, bool flag) {
+//         if (idx >= n) {
+//             return 0;
+//         }
 
-        if (dp[idx][flag] != -1) {
-            return dp[idx][flag];
-        }
+//         if (dp[idx][flag] != -1) {
+//             return dp[idx][flag];
+//         }
 
        
-        long long skip = solve(idx + 1, nums, flag);
+//         long long skip = solve(idx + 1, nums, flag);
 
         
-        long long val = nums[idx];
+//         long long val = nums[idx];
 
-        if (flag == false) {
-            val = -val;
-        }
+//         if (flag == false) {
+//             val = -val;
+//         }
 
-        long long take = val + solve(idx + 1, nums, !flag);
+//         long long take = val + solve(idx + 1, nums, !flag);
 
-        return dp[idx][flag] = max(take, skip);
-    }
+//         return dp[idx][flag] = max(take, skip);
+//     }
+
+//     long long maxAlternatingSum(vector<int>& nums) {
+//         n = nums.size();
+
+//         dp.assign(n, vector<long long>(2, -1));
+
+//         return solve(0, nums, true);
+//     }
+// };
+
+
+
+// BOTTOM UP APPROACH
+
+class Solution {
+public:
 
     long long maxAlternatingSum(vector<int>& nums) {
-        n = nums.size();
+        int n = nums.size();
 
-        dp.assign(n, vector<long long>(2, -1));
+        vector<vector<long long>> dp(n + 1, vector<long long>(2, 0));
 
-        return solve(0, nums, true);
+        for (int i = 1; i <= n; i++) {
+
+            dp[i][0] = max(dp[i-1][0],
+                           dp[i-1][1] + nums[i-1]);
+
+            
+            dp[i][1] = max(dp[i-1][1],
+                           dp[i-1][0] - nums[i-1]);
+        }
+
+        return dp[n][0];
     }
 };
