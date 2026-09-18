22ae94576ed0bf645d12afc71e2207abfdadbead
Stuti Chaurasia <166703911+stutichaurasia3014@users.noreply.github.com>
2026-09-18 15:38:01 +0530

    Time: 27 ms (58.74%) | Memory: 27.6 MB (18.78%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,27 +1,58 @@
+// class Solution {
+// public:
+// int m,n;
+// vector<vector<int>> dp;
+// int solve(string &text1,string &text2,int i,int j){
+//     if(i>=m || j>=n){
+//         return 0;
+//     }
+//      if(dp[i][j] !=-1){
+//         return dp[i][j];
+//      }
+//     if(text1[i]==text2[j]){
+//        return dp[i][j]=1+solve(text1,text2,i+1,j+1);
+//     }
+
+//     int a=solve(text1,text2,i+1,j);
+//     int b=solve(text1,text2,i,j+1);
+
+//     return dp[i][j]= max(a,b);
+// }
+//     int longestCommonSubsequence(string text1, string text2) {
+//         m=text1.length();
+//         n=text2.length();
+//         dp.assign(m,vector<int>(n,-1));
+//         return solve(text1,text2,0,0);
+//     }
+// };
+
+
 class Solution {
 public:
-int m,n;
-vector<vector<int>> dp;
-int solve(string &text1,string &text2,int i,int j){
-    if(i>=m || j>=n){
-        return 0;
-    }
-     if(dp[i][j] !=-1){
-        return dp[i][j];
-     }
-    if(text1[i]==text2[j]){
-       return dp[i][j]=1+solve(text1,text2,i+1,j+1);
-    }
 
-    int a=solve(text1,text2,i+1,j);
-    int b=solve(text1,text2,i,j+1);
-
-    return dp[i][j]= max(a,b);
-}
     int longestCommonSubsequence(string text1, string text2) {
-        m=text1.length();
-        n=text2.length();
-        dp.assign(m,vector<int>(n,-1));
-        return solve(text1,text2,0,0);
+       int m=text1.length();
+       int n=text2.length();
+      vector<vector<int>> dp(m+1,vector<int>(n+1));
+
+      for(int row=0;row<m+1;row++){
+        dp[row][0]=0;
+      }
+        for(int col=0;col<n+1;col++){
+           dp[0][col]=0;
+        }
+
+
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
