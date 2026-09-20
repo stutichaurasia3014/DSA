7eeae9648fe80e00ac2728259f36e621c3ab1086
Stuti Chaurasia <166703911+stutichaurasia3014@users.noreply.github.com>
2026-09-20 09:48:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.88%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int value = 'z' - s[i] + 1;
+            int position = i + 1;
+
+            ans += value * position;
+        }
+
+        return ans;
+    }
+};
