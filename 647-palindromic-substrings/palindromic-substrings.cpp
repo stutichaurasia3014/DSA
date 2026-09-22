78d90b36d316e226006324b2bff33660625336e2
Stuti Chaurasia <166703911+stutichaurasia3014@users.noreply.github.com>
2026-09-22 12:20:01 +0530

    Time: 27 ms (36.33%) | Memory: 13 MB (34.99%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
+int dp[1001][1001];
    bool check(string &s,int i,int j){
     if(i>=j){
         return true;
     }
-    if(s[i]==s[j]){
-        return check(s,i+1,j-1);
+    if(dp[i][j] !=-1){
+        return dp[i][j];
     }
-    return false;
+    if(s[i]==s[j]){
+        return dp[i][j]=check(s,i+1,j-1);
+    }
+    return dp[i][j]= false;
    }
     int countSubstrings(string s) {
         int n=s.length();
-
+        memset(dp,-1,sizeof(dp));
         int count=0;
 
         for(int i=0;i<n;i++){
