bbc1f3bb20fb84523dfd13c0307af8c57fb8c696
Stuti Chaurasia <166703911+stutichaurasia3014@users.noreply.github.com>
2026-09-22 13:09:07 +0530

    Time: 42 ms (24.72%) | Memory: 14.9 MB (34.45%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,3 +1,38 @@
+// class Solution {
+// public:
+// int dp[1001][1001];
+//    bool check(string &s,int i,int j){
+//     if(i>=j){
+//         return true;
+//     }
+//     if(dp[i][j] !=-1){
+//         return dp[i][j];
+//     }
+//     if(s[i]==s[j]){
+//         return dp[i][j]=check(s,i+1,j-1);
+//     }
+//     return dp[i][j]= false;
+//    }
+//     int countSubstrings(string s) {
+//         int n=s.length();
+//         memset(dp,-1,sizeof(dp));
+//         int count=0;
+
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 if(check(s,i,j)){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
 int dp[1001][1001];
@@ -15,14 +50,27 @@ int dp[1001][1001];
    }
     int countSubstrings(string s) {
         int n=s.length();
-        memset(dp,-1,sizeof(dp));
+       vector<vector<bool>> dp(n,vector<bool>(n,false));
         int count=0;
 
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(check(s,i,j)){
+        for(int L=1;L<=n;L++){
+            for(int i=0; i+L-1<n;i++){
+                int j=i+L-1;
+
+                if(i==j){
+                    dp[i][j]=true;
+                }
+                else if(i+1==j){
+                    dp[i][j]=(s[i]==s[j]);
+                }
+                else{
+                    dp[i][j]=(s[i]==s[j] && dp[i+1][j-1]); 
+                }
+
+                if(dp[i][j]==true){
                     count++;
                 }
+
             }
         }
         return count;
