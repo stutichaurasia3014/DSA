22d53f2d84d085b52d89cee600db7a8a99e3cd64
Stuti Chaurasia <166703911+stutichaurasia3014@users.noreply.github.com>
2026-09-22 11:58:38 +0530

    Time: 111 ms (5.07%) | Memory: 80.8 MB (9.70%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,28 +1,65 @@
+// class Solution {
+// public:
+// int m,n;
+// int dp[501][501];
+// int solve(string word1, string word2,int i,int j){
+//     if(i==m){
+//         return n-j;
+//     }
+//     if(j==n){
+//         return m-i;
+//     }
+
+//     if(dp[i][j]!=-1){
+//         return dp[i][j];
+//     }
+
+//     if(word1[i]==word2[j]){
+//         return dp[i][j]= solve(word1,word2,i+1,j+1);
+//     }
+//     else{
+//         int insertC=1+solve(word1,word2,i,j+1);
+//         int deleteC=1+solve(word1,word2,i+1,j);
+//         int replaceC=1+solve(word1,word2,i+1,j+1);
+
+//         return dp[i][j]= min({insertC,deleteC,replaceC});
+//     }
+//     return -1;
+// }
+//     int minDistance(string word1, string word2) {
+//         m=word1.length();
+//         n=word2.length();
+//        memset(dp,-1,sizeof(dp));
+//         return solve(word1,word2,0,0);
+//     }
+// };
+
+
+
+// traverse from right to left 
+
 class Solution {
 public:
 int m,n;
 int dp[501][501];
-int solve(string word1, string word2,int i,int j){
-    if(i==m){
-        return n-j;
-    }
-    if(j==n){
-        return m-i;
+int solve(string word1, string word2,int m,int n){
+    if(m==0 ||n==0){
+        return m+n;
     }
 
-    if(dp[i][j]!=-1){
-        return dp[i][j];
+    if(dp[m][n]!=-1){
+        return dp[m][n];
     }
 
-    if(word1[i]==word2[j]){
-        return dp[i][j]= solve(word1,word2,i+1,j+1);
+    if(word1[m-1]==word2[n-1]){
+        return dp[m][n]= solve(word1,word2,m-1,n-1);
     }
     else{
-        int insertC=1+solve(word1,word2,i,j+1);
-        int deleteC=1+solve(word1,word2,i+1,j);
-        int replaceC=1+solve(word1,word2,i+1,j+1);
+        int insertC=1+solve(word1,word2,m,n-1);
+        int deleteC=1+solve(word1,word2,m-1,n);
+        int replaceC=1+solve(word1,word2,m-1,n-1);
 
-        return dp[i][j]= min({insertC,deleteC,replaceC});
+        return dp[m][n]= min({insertC,deleteC,replaceC});
     }
     return -1;
 }
@@ -30,6 +67,6 @@ int solve(string word1, string word2,int i,int j){
         m=word1.length();
         n=word2.length();
        memset(dp,-1,sizeof(dp));
-        return solve(word1,word2,0,0);
+        return solve(word1,word2,m,n);
     }
 };
